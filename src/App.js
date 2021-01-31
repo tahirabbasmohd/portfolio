6575a3dc6c7a84bb6f2b017be828310899777094
Tahir <mtahir1997@gmail.com>
2021-01-31 12:21:38 +0530

    added my-carousal title-message scroll-down componenet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import MyNavbar from './components/my-navbar/mynavbar.components'
+import MyNavbar from './components/my-navbar/mynavbar.components';
+import MyCarousal from './components/my-carousals/my-carousals.components';
+import TitleMessgae from "./components/title-message/title-message.components";
 import './App.css';
+
+
 
 const App=()=> {
   return (
     <div>
       <MyNavbar />
-      My Portfolio
+      <MyCarousal />
+      <TitleMessgae />
     </div>
   );
 }
